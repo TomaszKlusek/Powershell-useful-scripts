e849297c4a7548dcce39931cfd2384ba407a2590
Tommy <tomasz.klusek.tk@gmail.com>
2021-01-07 14:07:47 +0100

    Add files via upload

---
+++ ActiveDirectory_Adding_member_to_groups.ps1
@@ -0,0 +1,62 @@
+﻿<#
+.SYNOPSIS
+This script adds AD members to specific groups.
+
+.DESCRIPTION
+We can use the script to add existing member to particular groups in the domain environment.
+To run this script you need to have PS Active Directory modul.
+
+.PARAMETER Member
+Specifies the member to add to groups.
+
+.PARAMETER GroupName
+Specifies the names of the groups to which the AD member is to be added.
+
+.EXAMPLE
+.\ActiveDirectory_Adding_member_to_groups.ps1 -member user1 -groupname group1
+#>
+
+
+Param(
+    [string]$Member = (Read-Host "Enter the user to be added to the AD group[s]"),
+    [array]$GroupName = ((Read-Host "Enter a name for the group[s]") -split ",").trim()
+    )
+
+#Save credentials
+$cred = Get-Credential -Message "Enter your login to authorize the operation"
+
+#Checking if user exists in AD
+try
+{
+    write-host "User $((Get-ADUser $Member).samaccountname) exist in domain`n" -ForegroundColor Blue -ErrorAction Stop
+}
+catch{Write-Host "User $Member doesn't exis in domain. You will exit the program." -ForegroundColor Red;exit}
+
+#Check if AD group exists
+foreach($G in $GroupName)
+{
+    try
+    {
+        Write-Host "`nGroup $((Get-ADGroup $G).name) exist in domain" -ForegroundColor Blue -ErrorAction Stop
+    }
+    catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+    {Write-Host "`nGroup $G doesn't exist in domain. You will exit the program." -ForegroundColor Red;exit}
+}
+
+
+#Executing command
+foreach($G in $GroupName)
+{
+    try
+    { 
+        if($Member -in (Get-ADGroupMember $G).samaccountname)
+        {Write-Host "`nUser $Member already belongs to the group $G" -ForegroundColor Yellow}
+        else 
+        {
+            Add-ADPrincipalGroupMembership -Identity $Member -MemberOf $G -Credential $cred -ErrorAction stop
+            Write-Host "`nMember $Member was added to the group $G" -ForegroundColor Green
+        }
+    }
+    catch
+    {Write-Host "You are not authorized to perform the operation" -ForegroundColor red;exit}
+}
---
+++ ActiveDirectory_Remove_users.ps1
@@ -0,0 +1,66 @@
+﻿<#
+.SYNOPSIS
+Removing users from Active Directory
+
+.DESCRIPTION
+The script removes users from the domain environment
+
+.PARAMETER ADusers
+With this parameter, we specify the users we want to remove from the Active Directory
+
+.PARAMETER Log
+Optional parameter to create the log
+
+.EXAMPLE
+.\ActiveDirectory_Remove_users.ps1 -ADusers user1,user2 -log
+#>
+
+
+#Name of users
+Param ($ADusers = (Read-Host "Enter the users you want to remove. Separate users with commas.").split(",").trim(),
+[switch]$log)
+
+if($log)
+{
+    do{
+    $logPath = Read-Host "Enter the path where the log should be created"
+
+
+        $testPath = Test-Path $logPath -IsValid
+        if($testPath -eq $false)
+        {Write-Warning "The path specified is invalid"}
+    }
+    until($testPath -eq $true)
+
+}
+
+$cred = Get-Credential -Message "Enter your login to authorize the operation"
+
+$ADusers | ForEach-Object { 
+    if($_ -notin (Get-ADUser -Filter *).samaccountname)
+    {
+        Write-Warning "Users account $PSitem doesn't exist."
+    }
+
+    else 
+    {
+        try
+        {
+        #Executing command
+        Remove-ADUser -Identity $_ -Verbose -Credential $cred
+        Write-Host "User $_ was deleted from the domain" -ForegroundColor Blue
+
+        if($log)
+        {
+            Write-Output "$_" | Out-File $logPath -Append
+        }
+        }
+
+        catch
+        {Write-host "You are not authorized to perform the operation" -ForegroundColor Red}
+
+    }  
+}
+
+if($log)
+{Write-Host "Log with deleted accounts is in $logPath" -ForegroundColor Green}
