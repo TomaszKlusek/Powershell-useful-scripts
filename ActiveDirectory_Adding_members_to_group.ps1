a6fdf2934b0ed12ebb3623b86b0cce12eae74380
Tommy <tomasz.klusek.tk@gmail.com>
2021-01-08 12:10:54 +0100

    two new AD ps scripts

---
+++ ActiveDirectory_Adding_members_to_group.ps1
@@ -0,0 +1,45 @@
+﻿<#
+.SYNOPSIS
+This script adds the user[s] to an Active Directory group.
+
+.DESCRIPTION
+We can use the script to add existing members to particular group in the domain environment.
+
+.EXAMPLE
+.\ActiveDirectory_Adding_members_to_group.ps1
+#>
+
+
+#Name of group
+$GroupName = Read-Host "Enter the name of the AD group"
+
+#Check if group exists in Active Directory
+if($GroupName -notin (Get-ADGroup -Filter *).name)
+{Write-Host "Group $GroupName doesn't exist. You will exit the program." -ForegroundColor Red; exit}
+
+#Members
+$Members = ((Read-Host "Enter the members who are to belong to the group. Separate individual members with a comma.") -split ",").Trim()
+
+#Check if members exist in Active Directory
+foreach ($Member in $Members) 
+{try
+{
+    write-host "User $((Get-ADUser $Member).samaccountname) exist in domain`n" -ForegroundColor Blue -ErrorAction Stop
+}
+catch{Write-Host "User $Member doesn't exis in domain. You will exit the program." -ForegroundColor Red;exit}
+}
+
+#Credentials
+$cred = Get-Credential -Message "Enter your login to authorize the operation"
+
+#Executing command
+try
+{
+Add-ADGroupMember -Identity $GroupName -Members $Members -Credential $cred -ErrorAction Stop
+Write-Host "User[s] $($Members -join ", ") added to group $Groupname. Total number of members added: $($Members.count)" -ForegroundColor Green
+}
+catch
+{
+Write-Host "You are not authorized to perform the operation" -ForegroundColor red
+}
+
---
+++ ActiveDirectory_checking_logged_in_users_on_remote_computers.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.SYNOPSIS
+Quickly check who is logged in on the remote computer in Active Directory environment.
+
+.DESCRIPTION
+The script allows you to quickly check who is logged in to the remote computer[s] on the domain network in which you are.
+You must provide the name of the remote computer[s].
+#>
+
+#Enter Computers to check
+[array]$computers = ((Read-Host "Enter the name of AD remote computer[s]") -split ",").Trim()
+
+#Checking the availability of computers
+foreach ($c in $computers) {
+
+    #Checking if computers exist in the domain
+    if ($c -notin (Get-ADComputer -Filter *).name) {
+        write-host "Computer $c does not exist in the domain. Please remove the computer from the list and run the program again`n" -ForegroundColor Red; pause; exit
+    }
+    else {
+        Write-Host "Computer $c is in the domain" -ForegroundColor Green
+    }
+    
+    #Checking if the computer is online
+    $testCon = Test-Connection $c -Count 2 -Quiet
+
+    if ($testCon -eq $false) {
+        Write-Host "Computer $c not responding. Please remove the computer from the list and run the program again`n" -ForegroundColor Red; pause; exit
+    }
+
+    write-host "Computer $c is online`n" -ForegroundColor Green
+}
+
+#Executing command
+$computers | ForEach-Object {Write-Host "`n $_" -ForegroundColor green; Invoke-Command -ComputerName $_ -ScriptBlock {quser}}
